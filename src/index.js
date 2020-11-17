3a2d0211098d8f7088d0a332e231c4a28c0455d1
David Conde Marin <davidmarin@rentalcars.com>
2020-11-17 17:26:26 +0000

    Fixing export issue

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const BodyParser = require('./parsing/body-parser');
 const ResponseUtil = require('./response/api-responses');
 const Security = require('./security/index');
+const getLogger = require('./logger/logger')
 
 module.exports = {
     BodyParser,
     ResponseUtil,
-    Security
+    Security,
+    getLogger
 };
